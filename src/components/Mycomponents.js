308401248a98b238e0c4ddad74e0e57fa2cb33f6
Manhcuong246 <cuongnmph52517@gmail.com>
2024-12-21 08:32:02 +0700

    video 16

---
+++ src/components/Mycomponents.js
@@ -0,0 +1,13 @@
+import React from "react";
+class Mycomponent extends React.Component {
+    render(){
+        return(
+
+            <div>
+                my firts components <br></br>
+                {Math.random()}
+            </div>
+        );
+    }
+}
+export default Mycomponent;
